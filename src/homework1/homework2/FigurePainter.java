72b2d7370980e06b573c3575d811cf31a8504b26
= <=>
2025-09-13 14:48:08 +0300

    figureP

--- src/homework1/homework2/FigurePainter.java
+++ src/homework1/homework2/FigurePainter.java
@@ -27,50 +27,52 @@ public class FigurePainter {
             for (int j = 4; j > i; j--) {
                 System.out.print(" ");
             }
-            for (int k = 0; k <=i;k ++) {
+            for (int k = 0; k <= i; k++) {
                 System.out.print("*");
 
             }
             System.out.println();
-            }
+        }
         System.out.println();
         //4
-        for (int i = 5; i >0; i--) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" *");
-
-            }
-            for (int j = 5; j >i ; j--) {
+        for (int i = 5; i > 0; i--) {
+            for (int j = 5; j > i; j--) {
                 System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
 
             }
-
             System.out.println();
         }
+        System.out.println();
         //5
         for (int i = 0; i < 5; i++) {
             for (int j = 4; j > i; j--) {
                 System.out.print(" ");
             }
-            for (int k = 0; k <=i;k ++) {
+            for (int k = 0; k <= i; k++) {
                 System.out.print(" * ");
 
             }
             System.out.println();
         }
         for (int i = 4; i >0 ; i--) {
-            for (int j = 0; j <i ; j++) {
-                System.out.print(" * ");
-
-            }
-            for (int j = 4; j >i ; j--) {
+            for (int j = 4; j >=i ; j--) {
                 System.out.print(" ");
 
+
+                }
+            for (int k = 0; k < i; k++) {
+                    System.out.print(" * ");
             }
             System.out.println();
         }
 
-
-        }
     }
+}
+
+
+
+
 
