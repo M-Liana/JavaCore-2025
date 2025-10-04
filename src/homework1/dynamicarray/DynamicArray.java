05cbab516d9a348756332fe1559d4b7addf39cab
= <=>
2025-10-04 20:21:03 +0300

    dynamicA

--- src/homework1/dynamicarray/DynamicArray.java
+++ src/homework1/dynamicarray/DynamicArray.java
@@ -6,33 +6,34 @@ public class DynamicArray {
     private int size = 0;
 
     public void add(int value) {
+
         if (size == array.length) {
             extend();
         }
         array[size++] = value;
     }
 
-    private void extend() {
-        int []tmp= new int[array.length+10];
+    private void extend(){
+        int[]tmp=new int[array.length+10];
         for (int i = 0; i < size; i++) {
-            tmp[i]=array[i];
+            tmp[i]= array[i];
 
         }
         array=tmp;
-
     }
-    public int getByIndex(int index) {
-        if(index<0||index>size){
-            System.out.println("wrong index ");
+    public int getByIndex(int index){
+        if (index < 0 || index > size) {
+            System.out.println("wrong index");
             return 0;
         }
         return array[index];
     }
     public void print(){
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i <size ; i++) {
             System.out.print(array[i]+" ");
 
         }
     }
 }
+
 
--- src/homework1/dynamicarray/DynamicArrayDemo.java
+++ src/homework1/dynamicarray/DynamicArrayDemo.java
@@ -3,23 +3,27 @@ package homework1.dynamicarray;
 public class DynamicArrayDemo {
 
     public static void main(String[] args) {
-DynamicArray dy = new DynamicArray();
-dy.add(1);
-dy.add(2);
-dy.add(33);
-dy.add(33);
-dy.add(33);
-dy.add(55);
-dy.add(56);
-dy.add(88);
-dy.add(88);
-dy.add(8899);
-dy.print();
-        System.out.println();
-        dy.add(8899);
-        dy.add(8899);
-        dy.add(8899);
+        DynamicArray dy =new DynamicArray();
+        dy.add(44);
+        dy.add(98);
+        dy.add(55);
+        dy.add(66);
+        dy.add(66);
+        dy.add(66);
+        dy.add(28);
         dy.print();
+        System.out.println();
+        dy.add(28);
+        dy.add(28);
+        dy.add(99);
+        dy.add(99);
+        dy.add(100);
+        dy.print();
+        System.out.println();
+        System.out.println(dy.getByIndex(6));
+        System.out.println(dy.getByIndex(466));
+
+
 
     }
 }
